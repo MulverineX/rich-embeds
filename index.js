ab2350a4deeb2486b2e6db96208bfeda5eae2e0f
Mulverine Gaming <mulverin3@gmail.com>
2020-11-09 09:20:39 -0700

    code that does not work 🎉

--- index.js
+++ index.js
@@ -4,38 +4,28 @@ const { getModule, getModuleByDisplayName, React } = require('powercord/webpack'
 
 const link_selector = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/;
 
-const EmbedPatcher = require('./components/EmbedPatcher');
-const EmbedsRenderer = require('./components/EmbedsRenderer');
+//const EmbedPatcher = require('./components/EmbedPatcher');
+//const EmbedsRenderer = require('./components/EmbedsRenderer');
 
 module.exports = class RichEmbeds extends Plugin {
   async startPlugin () {
-    const MessageContent = await getModule(m => m.type && m.type.displayName === 'MessageContent');
-    const Embed = (await getModuleByDisplayName('Embed')).prototype;
-    const mcType = MessageContent.type;
+    const { MessageAccessories } = await getModule(['MessageAccessories']);
+    const maType = MessageAccessories.type;
 
-    inject('rich-embeds-patched', Embed, 'render', function (_, res) {
-      const { embed } = this.props;
-      if (embed.url?.startsWith('https://github.com')) {
-        console.log(embed);
-      }
+    console.log(maType);
+
+    inject('rich-embeds-accessories', MessageAccessories, 'type', (args, res) => {
+      console.log(args);
+      console.log(res);
+
       return res;
     });
+    Object.assign(MessageAccessories.type, maType);
 
-    inject('rich-embeds-custom', MessageContent, 'type', (args, res) => {
-      if (link_selector.test(args[0].message.content)) {
-        res.props.children = React.createElement(EmbedsRenderer, {
-          original: res.props.children,
-          content: args[0].message.content
-        });
-      }
-
-      return res;
-    })
-    Object.assign(MessageContent.type, mcType);
+    console.log(MessageAccessories);
   }
 
   pluginWillUnload () {
-    uninject('rich-embeds-patched');
-    uninject('rich-embeds-custom');
+    uninject('rich-embeds-accessories');
   }
 };
