10000639853e1ebeb5f58c9027c998e9aca622f5
Mulverine Gaming <mulverin3@gmail.com>
2020-12-02 00:00:24 -0700

    Cleaning, un-stupified file size, started svg's

--- components/RenderFile.jsx
+++ components/RenderFile.jsx
@@ -1,4 +1,4 @@
-const { getModule, React } = require('powercord/webpack');
+const { getModule, getModuleByDisplayName, React } = require('powercord/webpack');
 
 const { Tooltip } = require('powercord/components');
 
@@ -7,24 +7,6 @@ const mime = require('mime-types');
 const Archive = require('./files/Archive'),
       Audio =   require('./files/Audio'),
       Vector =  require('./files/Vector');
-
-function roundTo(n, digits) {
-  var negative = false;
-  if (digits === undefined) {
-      digits = 0;
-  }
-  if (n < 0) {
-      negative = true;
-      n = n * -1;
-  }
-  var multiplicator = Math.pow(10, digits);
-  n = parseFloat((n * multiplicator).toFixed(11));
-  n = (Math.round(n) / multiplicator).toFixed(digits);
-  if (negative) {
-      n = (n * -1).toFixed(digits);
-  }
-  return n;
-}
 
 module.exports = class RichFile extends React.Component {
   constructor(props) { super(props); this.state = {} }
@@ -35,51 +17,52 @@ module.exports = class RichFile extends React.Component {
 
   render() {
     if (!this.props.embed.height) {
-      const { username, timestamp } = getModule([ 'systemMessageAccessories' ], false);
+      const Image = getModuleByDisplayName('LazyImageZoomable', false);
 
+      const { filesize } = getModule(['numberFormat'], false);
 
-      const proxy_url = this.props.embed.proxy_url.split('/');
+      const Style = getModule([ 'systemMessageAccessories' ], false);
 
-      const file_name = _.last(proxy_url);
+      const button = (title, run = false) => {return (
+              <Tooltip position='top' text={title.charAt(0).toUpperCase() + title.slice(1)}>
+                <div className={`re-toolbar-item${run ? ' re-clickable' : ''}`} onClick={run}>
+                  <div className={`re-icon re-icon-${title}`}></div><div></div>
+                </div>
+              </Tooltip>
+            )},
+            proxy_url = (this.props.embed.proxy_url || '/unknown-file'),
+            file_name = _.last(proxy_url.split('/')),
+            mime_type = file_name.includes('.') ? 
+              (() => {
+                try { return mime.lookup(file_name)?.split('/') } 
+                catch (e) { return false } }
+              )() : false,
+            file_size = filesize(this.props.embed.size);
 
-      const mime_type = file_name.includes('.') ? (() => { try { return mime.lookup(file_name)?.split('/') } catch (e) { return false } })() : false;
-
-      const size = this.props.embed.size;
-
-      const kb = roundTo((size / 1024), 2);
-
-      let b = false;
-      let mb = false;
-
-      if (kb < 1) b = size;
-      if (kb >= 1000) mb = roundTo((kb / 1024), 2);
-
-      const file_size = b ? `${b} bytes` : mb ? `${mb} MB` : `${kb} KB`;
-
+      // for anyone wondering no, I'm not dumb, there will be more special casing like this later
       switch (_.last(file_name.split('.'))) {
         case 'zip': mime_type[0] = 'archive'; break;
       }
 
-      let button = (title, run = false) => {
-        return (
-          <Tooltip position='top' text={title.charAt(0).toUpperCase() + title.slice(1)}>
-            <div className={`re-toolbar-item ${run ? 're-clickable' : ''}`} onClick={run}>
-              <div className={`re-icon re-icon-${title}`}></div><div></div>
-            </div>
-          </Tooltip>
-        )
-      }
-
       return (<div className='re-file'>
         <div className='re-file-header'>
-          <span className={`re-file-name ${username}`}>{file_name}</span>
-          <span className={`re-file-size ${timestamp}`}>{file_size}</span>
+          <span className={`re-file-name ${Style.username}`}>{file_name}</span>
+          <span className={`re-file-size ${Style.timestamp}`}>{file_size}</span>
         </div>
         <div className='re-toolbar'>
           { button(mime_type ? mime_type[0] : 'file') }
           { button('save', () => window.open(this.props.embed.url)) }
           { button('more', () => {}) }
         </div>
+        {/*
+          mime_type && mime_type[0] === 'image' ? React.createElement(Image, {
+            src: proxy_url,
+            original: this.props.embed.url,
+            className: 're-image',
+            height: 512,
+            width: 512
+          }) : false
+        */false}
       </div>)
     }
     else return this.props.content;
